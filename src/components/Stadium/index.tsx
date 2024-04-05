5564124e92840410828250c5b3c1b78dc17a617a
marwensouissi <89973135+marwensouissi@users.noreply.github.com>
2024-04-05 03:26:26 +0100

    Update index.tsx

--- src/components/Stadium/index.tsx
+++ src/components/Stadium/index.tsx
@@ -1,51 +1,46 @@
 // components/Stadium/index.tsx
 'use client'
+
 import React, { useState, useEffect } from 'react';
 import StadiumService, { Stadium } from '@/services/stadium/stadiumService';
 import Link from 'next/link';
 
+
 const StadiumList: React.FC = () => {
   const [stadiums, setStadiums] = useState<Stadium[]>([]);
-  const [locationNames, setLocationNames] = useState<string[]>([]);
+
+  const fetchStadiums = async () => {
+    try {
+      const data = await StadiumService.getAllStadiums();
+      setStadiums(data);
+    } catch (error) {
+      console.error('Error fetching stadiums:', error);
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await StadiumService.getAllStadiums();
-        setStadiums(data);
-
-        // Extract latitude and longitude from the location field for each stadium
-        const coordinates = data.map(stadium => stadium.location.split(',').map(parseFloat));
-        getLocationNames(coordinates);
-      } catch (error) {
-        console.error('Error fetching stadiums:', error);
-      }
-    };
-
-    fetchData();
+    fetchStadiums();
   }, []);
 
-  const getLocationNames = async (coordinates: number[][]) => {
-    try {
-      const names = await Promise.all(coordinates.map(async (coordinate) => {
-        const [latitude, longitude] = coordinate;
-        const response = await fetch(`https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`);
-        const data = await response.json();
-        if (data.error) {
-          console.error('Error fetching address:', data.error.message);
-          return '';
-        }
-        const address = data.display_name;
-        return address;
-      }));
-      setLocationNames(names);
-    } catch (error) {
-      console.error('Error fetching address:', error);
+
+
+
+
+  const handleDeleteStadium = async (id: string) => {
+    const confirmDelete = window.confirm('Are you sure you want to delete this stadium?');
+    if (confirmDelete) {
+      try {
+        await StadiumService.deleteStadium(id);
+        setStadiums(stadiums.filter(stadium => stadium._id !== id));
+      } catch (error) {
+        console.error('Error deleting stadium:', error);
+      }
     }
   };
 
   return (
     <div>
+
       <h1 className="text-2xl font-bold mb-4">Stadiums</h1>
 
       {/* Stadium List */}
@@ -57,15 +52,36 @@ const StadiumList: React.FC = () => {
               <th className="py-2 px-4 border-b">Location</th>
               <th className="py-2 px-4 border-b">Capacity</th>
               <th className="py-2 px-4 border-b">Available</th>
+              <th className="py-2 px-4 border-b">Actions</th>
             </tr>
           </thead>
           <tbody>
-            {stadiums.map((stadium, index) => (
+            {stadiums.map((stadium) => (
               <tr key={stadium._id}>
                 <td className="py-2 px-4 border-b">{stadium.name}</td>
-                <td className="py-2 px-4 border-b">{locationNames[index]}</td>
+                <td className="py-2 px-4 border-b"> {stadium.address} </td>
                 <td className="py-2 px-4 border-b">{stadium.capacity}</td>
                 <td className="py-2 px-4 border-b">{stadium.isAvailable ? 'Yes' : 'No'}</td>
+                <td className="py-2 px-4 border-b flex space-x-2">
+                  <Link href={`/stadium/${stadium._id}`}>
+                    <button className=" text-white px-4 py-2 rounded hover:bg-blue-700 mr-2" style={{ backgroundColor: '#1B998B' }}
+>
+                      View
+                    </button>
+                  </Link>
+                  <Link href={`/stadium/update/${stadium._id}`}>
+                    <button className=" text-white px-4 py-2 rounded hover:bg-yellow-700 mr-2" style={{ backgroundColor: '#0B4F6C' }} >
+                      Edit
+                    </button>
+                  </Link>
+                  <button
+                    onClick={() => handleDeleteStadium(stadium._id)}
+                    className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700 mr-2"
+                    
+                  >
+                    Delete
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
